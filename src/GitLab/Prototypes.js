a90d56309942d5fb501030e476086cac31a21681
Руслан Розбицкий <rosbitskyy@gmail.com>
2025-02-27 17:30:30 +0200

    Refactor String prototype extension for reusability.
    Replaced direct prototype modification with a reusable function, `defineObjectPrototype`. This ensures cleaner, more modular code with better error handling and prevents accidental overwrites while adding methods.

--- src/GitLab/Prototypes.js
+++ src/GitLab/Prototypes.js
@@ -8,31 +8,39 @@
  * @license Licensed under the MIT License (MIT)
  */
 
-/**
- * string prototypes
- */
+"use strict";
 
-(/**
- * Extends the String prototype by adding a `capitalize` method.
- *
- * The `capitalize` method transforms the first character of the string to uppercase
- * and retains the rest of the characters without any change.
- *
- * This extension is conditionally added to the String prototype only if it
- * does not already exist to avoid overwriting any existing implementation.
- */
-    () => {
-    "use strict";
-    const _ = String.prototype;
-    if (!Object.prototype.hasOwnProperty.call(_, 'capitalize')) {
-        /**
-         * Capitalizes the first character of the string and returns the modified string.
-         *
-         * @return {string} A new string with the first character converted to uppercase
-         *                  and the rest of the string unchanged.
-         */
-        _.capitalize = function () {
-            return this.charAt(0).toUpperCase() + this.slice(1);
-        }
+const defineObjectPrototype = (constructor, methodName, method) => {
+    // Перевірка, чи є переданий параметр конструктором (функцією)
+    if (typeof constructor !== 'function') {
+        throw new Error('Provided object must be a constructor function.');
     }
-})();
+    // Перевірка, чи передана назва методу є рядком
+    if (typeof methodName !== 'string') {
+        throw new Error('Cannot define method. Method name must be a string.');
+    }
+    // Перевірка, чи переданий метод є функцією
+    if (typeof method !== 'function') {
+        throw new Error('Cannot define method. Provided method is not a function.');
+    }
+
+    // Доступ до прототипу конструктора
+    const prototype = constructor.prototype;
+
+    // Перевірка, чи метод вже існує
+    if (!prototype.hasOwnProperty(methodName)) {
+        Object.defineProperty(prototype, methodName, {
+            value: method,
+            writable: false,
+            configurable: true,
+            enumerable: false, // метод не буде видно при ітерації через Object.keys
+        });
+        return true; // Успішне додавання
+    } else {
+        return false; // Метод вже існує
+    }
+};
+
+defineObjectPrototype(String, 'capitalize', function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+});
